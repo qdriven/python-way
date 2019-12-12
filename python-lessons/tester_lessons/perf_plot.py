5e6700f47382985038282e76157d6e92d87742f4
patrick <patrickwuke@163.com>
2019-12-12 14:09:19 +0800

    build chart

--- python-lessons/tester_lessons/perf_plot.py
+++ python-lessons/tester_lessons/perf_plot.py
@@ -17,6 +17,7 @@ import datetime
 from collections import namedtuple
 
 from openpyxl import Workbook
+from openpyxl.chart import LineChart, Reference
 
 MetricSample = namedtuple('MetricsData', 'start_time height num_tx')
 
@@ -35,8 +36,8 @@ def read_metric_file(path="metrics.log"):
             height = parsed_data[-2:][0].split("=")[1][0]
             num_tx = parsed_data[-1:][0].split("=")[1]
             metrics_sample = MetricSample(start_time=start_time,
-                                      height= int(height),
-                                      num_tx = int(num_tx))
+                                          height=int(height),
+                                          num_tx=int(num_tx))
             metrics_data.append(metrics_sample)
         except IndexError as e:
             pass
@@ -44,30 +45,53 @@ def read_metric_file(path="metrics.log"):
     return metrics_data
 
 
-def metrics_to_excel(metrics,excel_path="metrics.xlsx"):
+def metrics_to_excel(metrics, excel_path="metrics.xlsx"):
     wb = Workbook()
-    ws=wb.create_sheet("metric",0)
-    ws.cell(1,1,"duration")
-    ws.cell(1,2,"tps")
-    ws.cell(1,3,"height")
-    ws.cell(1,4,"num_tx")
+    ws = wb.create_sheet("metric", 0)
+    ws.cell(1, 1, "duration")
+    ws.cell(1, 2, "time_offset")
+    ws.cell(1, 3, "tps")
+    ws.cell(1, 4, "height")
+    ws.cell(1, 5, "num_tx")
     row_num = len(metrics)
+    time_offset = 0
     for index in range(row_num):
-        ws.cell(index + 2, 3, metrics[index].height)
-        ws.cell(index + 2, 4, metrics[index].num_tx)
-        if index ==0:
-            ws.cell(index+2,1,0)
-            ws.cell(index+2,2,0)
+        ws.cell(index + 2, 4, metrics[index].height)
+        ws.cell(index + 2, 5, metrics[index].num_tx)
+        if index == 0:
+            ws.cell(index + 2, 1, 0)
+            ws.cell(index + 2, 2, 0)
+            ws.cell(index + 2, 3, 0)
         else:
-            start_time = datetime.datetime.strptime(metrics_data[index -1].start_time,
-                                           "%Y/%m/%d %H:%M:%S.%f")
+            start_time = datetime.datetime.strptime(metrics_data[index - 1].start_time,
+                                                    "%Y/%m/%d %H:%M:%S.%f")
 
             end_time = datetime.datetime.strptime(metrics_data[index].start_time,
-                                   "%Y/%m/%d %H:%M:%S.%f")
-            duration = (end_time-start_time).seconds
-            ws.cell(index+2,1,duration)
-            ws.cell(index+2,2,metrics[index].num_tx/duration)
+                                                  "%Y/%m/%d %H:%M:%S.%f")
+            duration = (end_time - start_time).seconds
+            time_offset = time_offset + duration
+            ws.cell(index + 2, 1, duration)
+            ws.cell(index + 2, 2, time_offset)
+            ws.cell(index + 2, 3, metrics[index].num_tx / duration)
+    add_chart(ws, row_num)
     wb.save(excel_path)
+
+
+def add_chart(ws, max_row):
+    lc = LineChart()
+    lc.title = "Chain TPS"
+    lc.style = 15
+    lc.y_axis.title = "TPS"
+    lc.x_axis.title = "Time"
+    data = Reference(ws, min_col=3, min_row=1,
+                     max_col=3, max_row=max_row)
+    lc.add_data(data, titles_from_data=True)
+    x_timeoffset = Reference(ws,min_col=2,
+                                max_col =2,
+                                min_row=2,
+                                max_row=max_row)
+    lc.set_categories(x_timeoffset)
+    ws.add_chart(lc,"G13")
 
 
 if __name__ == '__main__':
